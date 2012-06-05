729a0896743c30bcb9d87441742bbc0a239994fb
Kaosphere <alex.jumeline@gmail.com>
2012-06-05 18:14:49 -0400

    gcc compilation and install

--- LFS system tools compilation and installation/Tools install.sh
+++ LFS system tools compilation and installation/Tools install.sh
@@ -13,4 +13,33 @@ cd ..
 rm -rf binutils-2.22
 rm -rf binutils-build
 
+#gcc compilation and installation
+tar -xf gcc-4.6.2.tar.bz2
+cd gcc-4.6.2
+tar -jxf ../mpfr-3.1.0.tar.bz2
+mv -v mpfr-3.1.0 mpfr
+tar -Jxf ../gmp-5.0.4.tar.xz
+mv -v gmp-5.0.4 gmp
+tar -zxf ../mpc-0.9.tar.gz
+mv -v mpc-0.9 mpc
+patch -Np1 -i ../gcc-4.6.2-cross_compile-1.patch
+mkdir -v ../gcc-build
+cd ../gcc-build
+../gcc-4.6.2/configure \
+--target=$LFS_TGT --prefix=/tools \
+--disable-nls --disable-shared --disable-multilib \
+--disable-decimal-float --disable-threads \
+--disable-libmudflap --disable-libssp \
+--disable-libgomp --disable-libquadmath \
+--disable-target-libiberty --disable-target-zlib \
+--enable-languages=c --without-ppl --without-cloog \
+--with-mpfr-include=$(pwd)/../gcc-4.6.2/mpfr/src \
+--with-mpfr-lib=$(pwd)/mpfr/src/.libs
+make
+make install
+ln -vs libgcc.a `$LFS_TGT-gcc -print-libgcc-file-name | \
+sed 's/libgcc/&_eh/'`
+
+
+
 
